24534babc8bfbc132f61bff62e13e3d053362fd1
Johir <56644201+Johir426@users.noreply.github.com>
2020-03-06 20:33:02 +0600

    Add files via upload

---
+++ java/java.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+ $(".head .menu ul .bar").click(function(){
+    $(".head .menu ul li").fadeIn(500);
+    $(".first-sec").css("margin-top","200px");
+    $(".first-sec").css("margin-top","200px");
+ }) 
+
+
+
+    });
